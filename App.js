ef486c32da228639b042d140f7c5625c5a11b71f
kiloa44 <khaledalmansor888@gmail.com>
2020-04-01 15:32:24 +0300

    login and signup

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#00ff11',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+var firebaseConfig = {
+    apiKey: "AIzaSyDG1I6IHA2gd2a4916Y5kW65xWmFoZRqPw",
+    authDomain: "voluwork-1.firebaseapp.com",
+    databaseURL: "https://voluwork-1.firebaseio.com",
+    projectId: "voluwork-1",
+    storageBucket: "voluwork-1.appspot.com",
+    messagingSenderId: "104135268148",
+    appId: "1:104135268148:web:e3faf9859dc6035e7eb761",
+    measurementId: "G-VN76K92826"
+  };
+  firebase.initializeApp(firebaseConfig);
